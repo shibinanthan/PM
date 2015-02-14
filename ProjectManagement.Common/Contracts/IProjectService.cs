6189f2a3b11801f439b68ba3b78882cd06d32baa
mahesh <shibinanthan.m@gmail.com>
2015-02-14 21:01:39 +0530

    Added test contract

---
+++ ProjectManagement.Common/Contracts/IProjectService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace ProjectManagement.Common.Contracts
+{
+    [ServiceContract]
+    interface IProjectService
+    {
+        [OperationContract(Name = "ValidateUser")]
+        bool ValidateUser(PMUser user);
+    }
+}
---
+++ ProjectManagement.Common/ProjectModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.Common
+{
+    class ProjectModel
+    {
+    }
+
+    public class PMUser : PMBaseModel
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int UserType { get; set; }
+    }
+
+    public class PMBaseModel
+    {
+        public string ConnectionString { get; set; }
+    }
+}
